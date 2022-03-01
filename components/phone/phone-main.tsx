c9e5690e534cbd8121659e8be47723a191a92421
exsvrm <91942847+exsvrm@users.noreply.github.com>
2022-03-01 23:08:45 +0100

    Update phone-main.tsx

--- components/phone/phone-main.tsx
+++ components/phone/phone-main.tsx
@@ -12,7 +12,7 @@ import Weather from "../app/weather/weather-main";
 
 import "./styles.scss";
 
-export const Time: React.FC = () => {const [date, setDate] = useState(new Date()); let dateInterval: number;const updateDate = () => setDate(new Date());useEffect(() => {var dateInterval = setInterval(updateDate, 10000); return () => {if (dateInterval) clearInterval(dateInterval)}; }, []); return ( <> {(date.getHours() < 10 ? "0" : "") + date.getHours()}:{(date.getMinutes() < 10 ? "0" : "") + date.getMinutes()} </> )}
+const Time: React.FC = () => {const [date, setDate] = useState(new Date()); let dateInterval: number;const updateDate = () => setDate(new Date());useEffect(() => {var dateInterval = setInterval(updateDate, 10000); return () => {if (dateInterval) clearInterval(dateInterval)}; }, []); return ( <> {(date.getHours() < 10 ? "0" : "") + date.getHours()}:{(date.getMinutes() < 10 ? "0" : "") + date.getMinutes()} </> )}
 
 const Phone = () => {
     return (
